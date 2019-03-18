5dde71b6ff46c591064acc7ef2f18b34c7a3323a
Uporold <46285458+Uporold@users.noreply.github.com>
2019-03-18 07:59:53 +0300

    Минимизация, фиксы
    Минимизация, фиксы

--- js/main.js
+++ js/main.js
@@ -1,84 +1,66 @@
 /* Попап */
 var link = document.querySelector(".map-btn");
-	var popup = document.querySelector(".popup-form-container");
-    var close = document.querySelector(".close-cross");
-    var form = popup.querySelector("form");
-    var login = popup.querySelector("[name = name]");
-    var email = popup.querySelector("[name=email]");
-    
-    var isStorageSupport = true;
-  var storage = "";
-  
-  try {
+var popup = document.querySelector(".popup-form-container");
+var close = document.querySelector(".close-cross");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name = name]");
+var email = popup.querySelector("[name = email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
     storage = localStorage.getItem("login");
-  } catch (err) {
+} catch (err) {
     isStorageSupport = false;
-  }
-    
-	link.addEventListener("click", function(evt){
-		evt.preventDefault();
-		popup.classList.add("show");
-        
-       if (storage) {
-      login.value = storage;
-      email.focus();
+}
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("show");
+
+    if (storage) {
+        login.value = storage;
+        email.focus();
     } else {
-      login.focus();
+        login.focus();
     }
-	});
-    
-    close.addEventListener("click", function(evt){
-		evt.preventDefault();
-        popup.classList.remove("show");
-        popup.classList.remove("modal-error");
-    });
-    
-     form.addEventListener("submit", function (evt) {
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("show");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
     if (!login.value || !email.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-         } else {
-      if (isStorageSupport) {
-        localStorage.setItem("login", login.value);
-      }
-    }
-  });
-    
-     window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("show")) {
-        popup.classList.remove("show");
+        evt.preventDefault();
         popup.classList.remove("modal-error");
-      }
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+        }
     }
-  });
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("show")) {
+            popup.classList.remove("show");
+            popup.classList.remove("modal-error");
+        }
+    }
+});
 
 /* Карта */
 
-/*var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
-
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [59.938639738117445, 30.32306045998567], // Москва
-        zoom: 17
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
-
-}*/
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
-            center: [59.93913800574294,30.321521561386636],
+            center: [59.93913800574294, 30.321521561386636],
             zoom: 17
         }, {
             searchControlProvider: 'yandex#search'
@@ -89,10 +71,8 @@ ymaps.ready(function () {
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         ),
 
-    
+        myPlacemarkWithContent = new ymaps.Placemark([59.93874546240775, 30.323177881614683], {
 
-        myPlacemarkWithContent = new ymaps.Placemark([59.93874546240775,30.323177881614683], {
-        
         }, {
             // Опции.
             // Необходимо указать данный тип макета.
@@ -105,7 +85,7 @@ ymaps.ready(function () {
             // её "ножки" (точки привязки).
             iconImageOffset: [-54, -193],
             // Смещение слоя с содержимым относительно слоя с картинкой.
-            
+
             // Макет содержимого.
             iconContentLayout: MyIconContentLayout
         });
